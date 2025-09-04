c2d96b1f0b5f65e8640a57aacb89ccfafc496864
Teslina(MamyNyAina) <tesligame@gmail.com>
2025-09-04 09:54:06 +0300

    mon new commit

--- examen/src/main/Eseignents.java
+++ examen/src/main/Eseignents.java
@@ -1,13 +1,16 @@
 package main;
 
 import java.time.Instant;
+import java.util.List;
 
 public class Eseignents extends Etudiant {
     private String specialite;
+    private List<Cours> matiere;
 
-    public Eseignents(int id, String nom, String prenom, Instant dateDeNaissance, String email, String telephone, String specialite) {
+    public Eseignents(int id, String nom, String prenom, Instant dateDeNaissance, String email, String telephone, String specialite, List<Cours> matiere) {
         super(id, nom, prenom, dateDeNaissance, email, telephone, null, null);
         this.specialite = specialite;
+        this.matiere = matiere;
     }
 
     public String getSpecialite() {
@@ -19,7 +22,7 @@ public class Eseignents extends Etudiant {
     }
 
     public static void main(String[] args) {
-        Eseignents Teacher1 = new Eseignents(1, "Smith", "Alice", java.time.Instant.parse("1975-03-15T08:00:00Z"), "azerty@mail.delta.com", "514-555-8765", "PROG2");
-        Eseignents Teacher2 = new Eseignents(2, "Johnson", "Bob", java.time.Instant.parse("1980-07-22T10:15:00Z"), "zeeee@mail.delta.com", "514-555-4321", "PROG1");
+        Eseignents Teacher1 = new Eseignents(1, "Smith", "Alice", java.time.Instant.parse("1975-03-15T08:00:00Z"), "azerty@mail.delta.com", "514-555-8765", "PROG2", "PROG2");
+        Eseignents Teacher2 = new Eseignents(2, "Johnson", "Bob", java.time.Instant.parse("1980-07-22T10:15:00Z"), "zeeee@mail.delta.com", "514-555-4321", "PROG1", "PROG1");
     }
 }
